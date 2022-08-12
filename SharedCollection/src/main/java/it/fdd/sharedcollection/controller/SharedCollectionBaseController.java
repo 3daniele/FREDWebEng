66b343d67b891e9607194cd1324c1f0d2ad148b7
3daniele <dev.danieledd@gmail.com>
2022-08-12 10:47:02 +0200

    SharedCollectionBaseController

---
+++ SharedCollection/src/main/java/it/fdd/sharedcollection/controller/SharedCollectionBaseController.java
@@ -0,0 +1,48 @@
+package it.fdd.sharedcollection.controller;
+
+import it.fdd.framework.result.FailureResult;
+import it.fdd.sharedcollection.data.dao.SharedCollectionDataLayer;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+
+public class SharedCollectionBaseController extends HttpServlet {
+    @Resource(name = "jdbc/FDDWebEng")
+    private DataSource ds;
+
+    protected abstract void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException;
+
+    private void processBaseRequest(HttpServletRequest request, HttpServletResponse response) {
+        //WARNING: never declare DB-related objects including references to Connection and Statement (as our data layer)
+        //as class variables of a servlet. Since servlet instances are reused, concurrent requests may conflict on such
+        //variables leading to unexpected results. To always have different connections and statements on a per-request
+        //(i.e., per-thread) basis, declare them in the doGet, doPost etc. (or in methods called by them) and
+        //(possibly) pass such variables through the request.
+        try (SharedCollectionDataLayer datalayer = new SharedCollectionDataLayer(ds)) {
+            datalayer.init();
+            request.setAttribute("datalayer", datalayer);
+            processRequest(request, response);
+        } catch (Exception ex) {
+            ex.printStackTrace(); //for debugging only
+            (new FailureResult(getServletContext())).activate(
+                    (ex.getMessage() != null || ex.getCause() == null) ? ex.getMessage() : ex.getCause().getMessage(), request, response);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processBaseRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processBaseRequest(request, response);
+    }
+}
