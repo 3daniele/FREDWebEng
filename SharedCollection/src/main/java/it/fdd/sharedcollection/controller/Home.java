1d6f48850395fb95b5553ae57d273719e0371bfd
3daniele <dev.danieledd@gmail.com>
2022-08-23 10:39:36 +0200

    template

---
+++ SharedCollection/src/main/java/it/fdd/sharedcollection/controller/Home.java
@@ -0,0 +1,70 @@
+package it.fdd.sharedcollection.controller;
+
+import it.fdd.framework.data.DataException;
+import it.fdd.framework.result.FailureResult;
+import it.fdd.framework.result.SplitSlashesFmkExt;
+import it.fdd.framework.result.TemplateManagerException;
+import it.fdd.framework.result.TemplateResult;
+import it.fdd.framework.security.SecurityLayer;
+import it.fdd.sharedcollection.data.dao.SharedCollectionDataLayer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Home extends SharedCollectionBaseController {
+    private String path="";
+
+    private void action_error(HttpServletRequest request, HttpServletResponse response) {
+        if (request.getAttribute("exception") != null) {
+            (new FailureResult(getServletContext())).activate((Exception) request.getAttribute("exception"), request, response);
+        } else {
+            (new FailureResult(getServletContext())).activate((String) request.getAttribute("message"), request, response);
+        }
+    }
+
+    private void action_default(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, TemplateManagerException {
+        TemplateResult res = new TemplateResult(getServletContext());
+        request.setAttribute("strip_slashes", new SplitSlashesFmkExt());
+        path = request.getRequestURL().toString();
+        if (path.equals("http://localhost:8080/SharedCollection_war/") || path.equals("http://localhost:8080/SharedCollection_war/home")){
+            request.setAttribute("display", true);
+        }else {
+            request.setAttribute("display", false);
+        }
+        //aggiungiamo al template un wrapper che ci permette di chiamare la funzione stripSlashes
+        //add to the template a wrapper object that allows to call the stripslashes function
+        res.activate("index.ftl.html", request, response);
+    }
+
+    @Override
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException {
+
+        try {
+
+            action_default(request, response);
+
+        } catch (NumberFormatException ex) {
+            request.setAttribute("message", "Invalid number submitted");
+            action_error(request, response);
+        } catch (IOException ex) {
+            request.setAttribute("exception", ex);
+            action_error(request, response);
+        } catch (TemplateManagerException ex) {
+            request.setAttribute("exception", ex);
+            action_error(request, response);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Write Article servlet";
+    }// </editor-fold>
+}
---
+++ SharedCollection/src/main/webapp/templates/error.ftl.html
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <title>Errore</title>
+        <link rel="stylesheet" href="style/default.css" type="text/css"/>
+    </head>
+    <body>
+        <div class="body">
+        <div><a href="issues">Return to homepage</a></div>
+        <h1>Error</h1>
+        <p>The system encountered the following error while handling your request:</p>
+        <p><b>${error!"Unknown error"}</b></p>
+        </div>
+    </body>
+</html>
