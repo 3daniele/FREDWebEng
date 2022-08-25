a8081647f9b65e233985969189f04e639cee7315
3daniele <dev.danieledd@gmail.com>
2022-08-25 16:45:00 +0200

    aggiornamento DB

--- Documentazione/FDDWebEng.sql
+++ Documentazione/FDDWebEng.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Creato il: Ago 01, 2022 alle 10:41
+-- Creato il: Ago 25, 2022 alle 14:34
 -- Versione del server: 5.7.34
 -- Versione PHP: 7.4.21
 
@@ -35,7 +35,16 @@ CREATE TABLE IF NOT EXISTS `Artista` (
   `cognome` varchar(100) NOT NULL,
   `nomeDArte` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Dump dei dati per la tabella `Artista`
+--
+
+INSERT INTO `Artista` (`id`, `nome`, `cognome`, `nomeDArte`) VALUES
+(1, 'Nelson', 'Venceslai', 'Rovere'),
+(2, 'Luciano', 'Ligabue', 'Ligabue'),
+(3, 'Riccardo', 'Zanotti', 'Pinguini tattici nucleari');
 
 -- --------------------------------------------------------
 
@@ -48,7 +57,39 @@ CREATE TABLE IF NOT EXISTS `Canzone` (
   `nome` varchar(100) NOT NULL,
   `durata` time NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+
+--
+-- Dump dei dati per la tabella `Canzone`
+--
+
+INSERT INTO `Canzone` (`id`, `nome`, `durata`) VALUES
+(1, 'caccia militare', '00:03:11'),
+(2, 'tadb', '00:03:09'),
+(3, 'soli come a bologna', '00:03:22'),
+(4, 'peter pan', '00:03:00'),
+(5, 'sport', '00:03:46'),
+(6, 'conforto', '00:02:09'),
+(7, 'silezio', '00:03:28'),
+(8, 'everest', '00:03:05'),
+(9, 'primavera 80', '00:03:00'),
+(10, 'la pioggia che non sapevo', '00:02:51'),
+(11, '321... major tom', '00:00:59'),
+(12, 'astronauta', '00:04:04'),
+(13, 'la libertà', '00:03:31'),
+(14, 'freddo cane', '00:03:08'),
+(15, 'looney', '00:03:25'),
+(16, 'lupo', '00:03:00'),
+(17, 'dalla terra a marte', '00:03:40'),
+(18, 'alveare', '00:03:20'),
+(19, 'martedì', '00:03:42'),
+(20, 'sentiero rapido', '00:04:14'),
+(21, 'bim bum bam', '00:03:18'),
+(22, 'mappamondo', '00:03:37'),
+(23, 'crescere', '00:03:46'),
+(24, 'precipitare', '00:05:13'),
+(25, 'Giovani Wannabe', '00:03:33'),
+(26, 'Dentista Croazia', '00:04:23');
 
 -- --------------------------------------------------------
 
@@ -64,7 +105,15 @@ CREATE TABLE IF NOT EXISTS `Collezione` (
   `utente` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `utente` (`utente`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Dump dei dati per la tabella `Collezione`
+--
+
+INSERT INTO `Collezione` (`id`, `nome`, `condivisione`, `dataDiCreazione`, `utente`) VALUES
+(1, 'Il meglio di Rovere', 'pubblica', '2022-08-12 09:04:05', 1),
+(3, 'Nuovi pinguini', 'privata', '2022-08-25 14:28:08', 2);
 
 -- --------------------------------------------------------
 
@@ -78,7 +127,17 @@ CREATE TABLE IF NOT EXISTS `Disco` (
   `etichetta` varchar(50) NOT NULL,
   `anno` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Dump dei dati per la tabella `Disco`
+--
+
+INSERT INTO `Disco` (`id`, `nome`, `etichetta`, `anno`) VALUES
+(1, 'Disponibile anche in mogano', 'Sony Music', '2019-03-29'),
+(2, 'Dalla terra a Marte', 'Sony Music', '2022-02-18'),
+(3, 'Dentista Croazia', 'Sony Music', '2022-08-24'),
+(4, 'Giovani Wannabe', 'Sony Music', '2022-06-15');
 
 -- --------------------------------------------------------
 
@@ -238,7 +297,39 @@ CREATE TABLE IF NOT EXISTS `ListaArtisti` (
   PRIMARY KEY (`id`),
   KEY `artista` (`artista`),
   KEY `canzone` (`canzone`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
+
+--
+-- Dump dei dati per la tabella `ListaArtisti`
+--
+
+INSERT INTO `ListaArtisti` (`id`, `artista`, `canzone`, `ruolo`) VALUES
+(1, 1, 1, 'Entrambi'),
+(2, 1, 2, 'Entrambi'),
+(3, 1, 3, 'Entrambi'),
+(4, 1, 4, 'Entrambi'),
+(5, 1, 5, 'Entrambi'),
+(6, 1, 6, 'Entrambi'),
+(7, 1, 7, 'Entrambi'),
+(8, 1, 8, 'Entrambi'),
+(9, 1, 9, 'Entrambi'),
+(10, 1, 10, 'Entrambi'),
+(11, 1, 11, 'Entrambi'),
+(12, 1, 12, 'Entrambi'),
+(13, 1, 13, 'Entrambi'),
+(14, 1, 14, 'Entrambi'),
+(15, 1, 15, 'Entrambi'),
+(16, 1, 16, 'Entrambi'),
+(17, 1, 17, 'Entrambi'),
+(18, 1, 18, 'Entrambi'),
+(19, 1, 19, 'Entrambi'),
+(20, 1, 20, 'Entrambi'),
+(21, 1, 21, 'Entrambi'),
+(22, 1, 22, 'Entrambi'),
+(23, 1, 23, 'Entrambi'),
+(24, 1, 24, 'Entrambi'),
+(25, 3, 25, 'Entrambi'),
+(26, 3, 26, 'Entrambi');
 
 -- --------------------------------------------------------
 
@@ -253,7 +344,39 @@ CREATE TABLE IF NOT EXISTS `ListaBrani` (
   PRIMARY KEY (`id`),
   KEY `canzone` (`canzone`),
   KEY `disco` (`disco`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+
+--
+-- Dump dei dati per la tabella `ListaBrani`
+--
+
+INSERT INTO `ListaBrani` (`id`, `disco`, `canzone`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 1, 3),
+(4, 1, 4),
+(5, 1, 5),
+(6, 1, 6),
+(7, 1, 7),
+(8, 1, 8),
+(9, 1, 9),
+(10, 1, 10),
+(11, 2, 11),
+(12, 2, 12),
+(13, 2, 13),
+(14, 2, 14),
+(15, 2, 15),
+(16, 2, 16),
+(17, 2, 17),
+(18, 2, 18),
+(19, 2, 19),
+(20, 2, 20),
+(21, 2, 21),
+(22, 2, 22),
+(23, 2, 23),
+(24, 2, 24),
+(25, 3, 26),
+(26, 4, 25);
 
 -- --------------------------------------------------------
 
@@ -277,7 +400,17 @@ CREATE TABLE IF NOT EXISTS `ListaDischi` (
   UNIQUE KEY `barcode` (`barcode`),
   KEY `collezione` (`collezione`),
   KEY `disco` (`disco`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Dump dei dati per la tabella `ListaDischi`
+--
+
+INSERT INTO `ListaDischi` (`id`, `collezione`, `disco`, `numeroCopie`, `Stato`, `formato`, `barcode`, `imgCopertina`, `imgFronte`, `imgRetro`, `imgLibretto`) VALUES
+(1, 1, 2, 5, 'Ottimo', 'Vinile', '0213456789', 'https://m.media-amazon.com/images/I/91iHBQuDJ-L._AC_SY450_.jpg', '', '', ''),
+(2, 1, 1, 8, 'Ottimo', 'CD', '0123456789', 'https://is3-ssl.mzstatic.com/image/thumb/Music113/v4/f2/68/aa/f268aa06-1da7-84e9-f7eb-384b9fbc7f27/886447616475.jpg/600x600bf-60.jpg', '', '', ''),
+(3, 3, 4, 2, 'Buono', 'Vinile', '0321456789', 'https://www.bergamonews.it/photogallery_new/images/2022/05/giovani-wannabe-il-nuovo-singolo-dei-pinguini-tattici-720072.large.jpg', NULL, NULL, NULL),
+(4, 3, 3, 10, 'Ottimo', 'CD', '0912345678', 'https://www.spettacoloitaliano.it/wp-content/uploads/2022/08/dentista-croazia-testo.jpg', NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -292,7 +425,39 @@ CREATE TABLE IF NOT EXISTS `ListaGeneri` (
   PRIMARY KEY (`id`),
   KEY `canzone` (`canzone`),
   KEY `genere` (`genere`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+
+--
+-- Dump dei dati per la tabella `ListaGeneri`
+--
+
+INSERT INTO `ListaGeneri` (`id`, `canzone`, `genere`) VALUES
+(1, 1, 60),
+(2, 2, 60),
+(3, 3, 60),
+(4, 4, 60),
+(5, 5, 60),
+(6, 6, 60),
+(7, 7, 60),
+(8, 8, 60),
+(9, 9, 60),
+(10, 10, 60),
+(11, 11, 60),
+(12, 12, 60),
+(13, 13, 60),
+(14, 14, 60),
+(15, 15, 60),
+(16, 16, 60),
+(17, 17, 60),
+(18, 18, 60),
+(19, 19, 60),
+(20, 20, 60),
+(21, 21, 60),
+(22, 22, 60),
+(23, 23, 60),
+(24, 24, 60),
+(25, 25, 60),
+(26, 26, 60);
 
 -- --------------------------------------------------------
 
@@ -309,7 +474,15 @@ CREATE TABLE IF NOT EXISTS `Utente` (
   `cognome` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `nickname` (`nickname`,`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+--
+-- Dump dei dati per la tabella `Utente`
+--
+
+INSERT INTO `Utente` (`id`, `nickname`, `email`, `password`, `nome`, `cognome`) VALUES
+(1, 'user', 'utente@utente.it', 'utenteutente', 'Utente', 'Utente'),
+(2, 'gestore', 'gestore@gestore.it', 'gestoregestore', 'Gestore', 'Gestore');
 
 -- --------------------------------------------------------
 
@@ -324,7 +497,14 @@ CREATE TABLE IF NOT EXISTS `UtentiAutorizzati` (
   PRIMARY KEY (`id`),
   KEY `collezione` (`collezione`),
   KEY `utente` (`utente`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Dump dei dati per la tabella `UtentiAutorizzati`
+--
+
+INSERT INTO `UtentiAutorizzati` (`id`, `collezione`, `utente`) VALUES
+(3, 3, 1);
 
 --
 -- Limiti per le tabelle scaricate
